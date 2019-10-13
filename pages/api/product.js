20335fccd2e1620d9942d5db6f56a017b2f0dc7d
dubemelchi <dubemelchi@gmail.com>
2019-10-13 03:47:47 -0400

    don't remember yikes

--- pages/api/product.js
+++ pages/api/product.js
@@ -1,18 +1,11 @@
 import Product from '../../models/Product';
 
 export default async (req, res) => {
-  const { _id } = req.query;
-  const product = await Product.findOne({ _id });
-  res.status(200).json(product);
-};
-import Product from "../../models/Product";
-
-export default async (req, res) => {
   switch (req.method) {
-    case "GET":
+    case 'GET':
       await handleGetRequest(req, res);
       break;
-    case "DELETE":
+    case 'DELETE':
       await handleDeleteRequest(req, res);
       break;
     default:
