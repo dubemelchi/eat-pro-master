25b3d3b1abbb3601dc94bbc83d869022f7400a94
dubemelchi <dubemelchi@gmail.com>
2019-10-19 18:51:04 -0400

    added checkout with stripe test api

--- next.config.js
+++ next.config.js
@@ -5,6 +5,6 @@ module.exports = {
       'mongodb+srv://meldee:mel1234@eat-2-fit-gofbv.mongodb.net/test?retryWrites=true&w=majority',
     JWT_SECRET: 'meldee',
     CLOUDINARY_URL: 'https://api.cloudinary.com/v1_1/drohkty8r/image/upload',
-    STRIPE_SECRET_KEY: '<insert here>'
+    STRIPE_SECRET_KEY: 'sk_test_EMH13rLZOyVYrltFQYoi63el00G1Zhopfb'
   }
 };
